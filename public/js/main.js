20b5e5966877b716d5730d73e5ca61df6dc8360d
Eduardo Ribeiro <joseribeiroejrs@ufu.br>
2017-12-20 21:04:06 -0200

    Adicionando os numeros de caracteres e palavras

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,13 @@ let frase = $(".frase").text();
 let tamanho = frase.split(" ").length;
 
 let numeroPalavrasText = $("#informacoes__numero--palavras")
-
 numeroPalavrasText.text(tamanho);
 
-console.log(tamanho);
+let campo = $("#campo-digitacao");
+campo.on("input", function(){
+    let numeroCaracteres = campo.val().length;
+    let numeroPalavras = campo.val().split(/\S+/).length - 1;
+
+    $("#campo-digitacao__numero--caracteres").text(numeroCaracteres);
+    $("#campo-digitacao__numero--palavras").text(numeroPalavras);
+});
